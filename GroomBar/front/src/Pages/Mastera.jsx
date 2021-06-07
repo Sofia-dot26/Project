8d318800756ee31cff82813855b1b02b29e1a449
Sofia-dot26 <62113915+Sofia-dot26@users.noreply.github.com>
2021-06-07 11:32:16 +0300

    Update Mastera.jsx

--- GroomBar/front/src/Pages/Mastera.jsx
+++ GroomBar/front/src/Pages/Mastera.jsx
@@ -21,35 +21,35 @@ function Mastera() {
 function Master() {
     return (
         <>
-            <section class="team">
+            <section className="team">
                 <h1>Мастера</h1>
-                <div class="container">
-                    <div class="team_inner">
+                <div className="container">
+                    <div className="team_inner">
 
-                        <div class="team_item">
-                            <img class="team_photo" src={team1}/> 
-                            <div class="team_name">Елизавета</div>
-                            <div class="team_text">Дипломированный кинолог. Елизавета с легкостью справляется с самыми
+                        <div className="team_item">
+                            <img className="team_photo" src={team1}/> 
+                            <div className="team_name">Елизавета</div>
+                            <div className="team_text">Дипломированный кинолог. Елизавета с легкостью справляется с самыми
                             крошечными и самыми большими четвероногими друзьями.</div>
                         </div>
 
-                        <div class="team_item">
-                            <img class="team_photo"src={team2}/>
-                            <div class="team_name">Алена</div>
-                            <div class="team_text">Выпускница Московской школы груминга. Всегда находит подход даже к самым
+                        <div className="team_item">
+                            <img className="team_photo"src={team2}/>
+                            <div className="team_name">Алена</div>
+                            <div className="team_text">Выпускница Московской школы груминга. Всегда находит подход даже к самым
                             строптивым клиентам!</div>
                         </div>
 
-                        <div class="team_item">
-                            <img class="team_photo" src={team3}/>
-                            <div class="team_name">Ольга</div>
-                            <div class="team_text">Ольга закончила Московскую школу груминга. Она очень любит животных и
+                        <div className="team_item">
+                            <img className="team_photo" src={team3}/>
+                            <div className="team_name">Ольга</div>
+                            <div className="team_text">Ольга закончила Московскую школу груминга. Она очень любит животных и
                             ответственно подходит к работе.</div>
                         </div>
-                        <div class="team_item">
-                            <img class="team_photo" src={team4}/>
-                            <div class="team_name">Анастасия</div>
-                            <div class="team_text">Закончила Московскую школу груминга. В ее нежных и добрых руках собакам
+                        <div className="team_item">
+                            <img className="team_photo" src={team4}/>
+                            <div className="team_name">Анастасия</div>
+                            <div className="team_text">Закончила Московскую школу груминга. В ее нежных и добрых руках собакам
                             уютно и комфортно.</div>
                         </div>
                     </div>
