7725346618d7c25d9c54bdb0c5de17eb2b84f84b
Sofia-dot26 <62113915+Sofia-dot26@users.noreply.github.com>
2021-06-07 11:36:23 +0300

    Update Uslugi.jsx

--- GroomBar/front/src/Pages/Uslugi.jsx
+++ GroomBar/front/src/Pages/Uslugi.jsx
@@ -18,8 +18,8 @@ function Table() {
     return (
         <>
             <section>
-                <div class="container">
-                    <div class="table">
+                <div className="container">
+                    <div className="table">
                         <h1>Стрижки для собак</h1>
                         <table>
                             <tbody>
@@ -36,48 +36,48 @@ function Table() {
                                 <tr>
                                     <td></td>
                                     <td width="300">Маленькие гладкошерстные породы</td>
-                                    <td class="ro">950 руб.</td>
-                                    <td class="ro">760 руб.</td>
-                                    <td class="ro">855 руб.</td>
-                                    <td class="ro">590 руб.</td>
+                                    <td className="ro">950 руб.</td>
+                                    <td className="ro">760 руб.</td>
+                                    <td className="ro">855 руб.</td>
+                                    <td className="ro">590 руб.</td>
                                 </tr>
                                 <tr>
                                     <td></td>
                                     <td>Маленькие длинношерстные породы</td>
-                                    <td class="ro">1 250 руб.</td>
-                                    <td class="ro">1 000 руб.</td>
-                                    <td class="ro">1 125 руб.</td>
-                                    <td class="ro">850 руб.</td>
+                                    <td className="ro">1 250 руб.</td>
+                                    <td className="ro">1 000 руб.</td>
+                                    <td className="ro">1 125 руб.</td>
+                                    <td className="ro">850 руб.</td>
                                 </tr>
                                 <tr>
                                     <td></td>
                                     <td>Средние породы</td>
-                                    <td class="ro">1400 - 2250 руб.</td>
-                                    <td class="ro">1120 - 1800 руб.</td>
-                                    <td class="ro">1260 - 2025 руб.</td>
-                                    <td class="ro">1 100 руб.</td>
+                                    <td className="ro">1400 - 2250 руб.</td>
+                                    <td className="ro">1120 - 1800 руб.</td>
+                                    <td className="ro">1260 - 2025 руб.</td>
+                                    <td className="ro">1 100 руб.</td>
                                 </tr>
                                 <tr>
                                     <td></td>
                                     <td>Крупные породы</td>
-                                    <td class="ro">от 1 690 руб.</td>
-                                    <td class="ro">от 1 360 руб.</td>
-                                    <td class="ro">1 520 руб.</td>
-                                    <td class="ro">1 500 руб.</td>
+                                    <td className="ro">от 1 690 руб.</td>
+                                    <td className="ro">от 1 360 руб.</td>
+                                    <td className="ro">1 520 руб.</td>
+                                    <td className="ro">1 500 руб.</td>
 
                                 </tr>
                             </tbody>
                         </table>
-                        <div class="p">*– 20% – от стоимости комплекса при повторном посещении салона в течение 30 дней</div>
-                        <div class="p">– 10% – от стоимости комплекса при повторном посещении салона в течение 60 дней</div>
-                        <div class="p">** – комплекс для щенков до 6 месячного возраста.</div>
-                        <div class="p">!Цена действительна при предъявлении вет. паспорта животного или щенячьей метрики. На данный комплекс скидки не действуют.</div>
+                        <div className="p">*– 20% – от стоимости комплекса при повторном посещении салона в течение 30 дней</div>
+                        <div className="p">– 10% – от стоимости комплекса при повторном посещении салона в течение 60 дней</div>
+                        <div className="p">** – комплекс для щенков до 6 месячного возраста.</div>
+                        <div className="p">!Цена действительна при предъявлении вет. паспорта животного или щенячьей метрики. На данный комплекс скидки не действуют.</div>
                     </div>
                 </div>
             </section>
             <section>
-                <div class="container">
-                    <div class="table">
+                <div className="container">
+                    <div className="table">
                         <h1>Стрижки для кошек</h1>
                         <table>
                             <tbody>
@@ -90,22 +90,22 @@ function Table() {
                                 <tr>
                                     <td></td>
                                     <td width="300">Комплекс "Стрижка кошек"</td>
-                                    <td class="ro">1 100 руб.</td>
+                                    <td className="ro">1 100 руб.</td>
                                 </tr>
                                 <tr>
                                     <td></td>
                                     <td>Комплекс "Стрижка Мейн-Куна"</td>
-                                    <td class="ro">1 250 руб.</td>
+                                    <td className="ro">1 250 руб.</td>
                                 </tr>
                                 <tr>
                                     <td></td>
                                     <td>Комплекс "Купание кошек"</td>
-                                    <td class="ro">890 руб.</td>
+                                    <td className="ro">890 руб.</td>
                                 </tr>
                                 <tr>
                                     <td></td>
                                     <td>Экспресс-линька</td>
-                                    <td class="ro">1 250 руб.</td>
+                                    <td className="ro">1 250 руб.</td>
 
                                 </tr>
                             </tbody>
@@ -114,25 +114,25 @@ function Table() {
                 </div>
             </section>
             <section>
-                <div class="container">
-                    <div class="table">
+                <div className="container">
+                    <div className="table">
                         <h1>Чистка зубов</h1>
                         <table>
                             <tbody>
                                 <tr>
                                     <td></td>
                                     <td width="300">Мелкие породы</td>
-                                    <td class="ro">1 300 руб.</td>
+                                    <td className="ro">1 300 руб.</td>
                                 </tr>
                                 <tr>
                                     <td></td>
                                     <td>Средние породы</td>
-                                    <td class="ro">1 500 руб.</td>
+                                    <td className="ro">1 500 руб.</td>
                                 </tr>
                                 <tr>
                                     <td></td>
                                     <td>Крупные породы</td>
-                                    <td class="ro">2 100 руб.</td>
+                                    <td className="ro">2 100 руб.</td>
                                 </tr>
 
                             </tbody>
@@ -141,36 +141,36 @@ function Table() {
                 </div>
             </section>
             <section>
-                <div class="container">
-                    <div class="table">
+                <div className="container">
+                    <div className="table">
                         <h1>Гигиенические Услуги</h1>
                         <table>
                             <tbody>
                                 <tr>
                                     <td></td>
                                     <td width="300">Чистка параанальных желез</td>
-                                    <td class="ro">200 руб.</td>
+                                    <td className="ro">200 руб.</td>
                                 </tr>
                                 <tr>
                                     <td></td>
                                     <td>Стрижка когтей</td>
-                                    <td class="ro">200 руб.</td>
+                                    <td className="ro">200 руб.</td>
                                 </tr>
                                 <tr>
                                     <td></td>
                                     <td>Стрижка когтей с полировкой</td>
-                                    <td class="ro">300 руб.</td>
+                                    <td className="ro">300 руб.</td>
                                 </tr>
                                 <tr>
                                     <td></td>
                                     <td>Установка антицарапок</td>
-                                    <td class="ro">375 руб.</td>
+                                    <td className="ro">375 руб.</td>
 
                                 </tr>
                                 <tr>
                                     <td></td>
                                     <td>Чистка ушек</td>
-                                    <td class="ro">150 руб.</td>
+                                    <td className="ro">150 руб.</td>
 
                                 </tr>
                             </tbody>
